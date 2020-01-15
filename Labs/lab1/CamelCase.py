69d3d7f4375c3f572f9c90725bc4a3b13870f910
nic <DominicB@email.com>
2020-01-15 15:32:38 -0600

    lab1 with project1 started ;) fixed camel string

--- Labs/lab1/CamelCase.py
+++ Labs/lab1/CamelCase.py
@@ -5,9 +5,9 @@ def getInput():
 def printOutput(words):
     for word in words :
         if (word != words[0]):
-            print(f'{word.title()}',end='')
+            print(word.title(),end='')
         else:
-            print(f'{word.lower()}',end='')
+            print(word.lower(),end='')
 # stuff
 if __name__ == '__main__':
     words=getInput()
